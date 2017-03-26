baf3163e7eadd21059f51fd23d167851586282a6
Armando Ordorica <armandordorica@gmail.com>
2017-03-26 17:34:48 +0000

    Generic initial setup for an app requiring mongoose, express and body parser

---
+++ RESTful/RESTfulBlogApp/app.js
@@ -0,0 +1,14 @@
+var bodyParser = require("body-parser");
+var mongoose       = require("mongoose");
+var express        = require("express");
+var app            = express();
+
+mongoose.connect("mongodb://localhost/restful_blog_app");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("SERVER IS RUNNING"); 
+});
